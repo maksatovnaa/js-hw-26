2dab9dec62a1db1a4b1c5703f7c8c89bb8d7d1ba
tazhmurat safiyanur <sakibaeva2024@MacBook-Air-Maksatovna.local>
2025-04-27 01:37:02 +0500

    first commit

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ content.style.backgroundImage = "url('bg-pattern-desktop.svg')";
 content.style.backgroundRepeat = ('no-repeat');
 content.style.padding = ('100px 200px');
 
-const heading = document.querySelector('h1');
-heading.style.fontSize = '70px';
-heading.style.textTransform = 'uppercase';
-heading.style.letterSpacing = '15px';
+const text = document.querySelector('h1');
+text.style.fontSize = '70px';
+text.style.textTransform = 'uppercase';
+text.style.letterSpacing = '15px';
 
 const title = document.querySelector('span')
 title.style.color = ('hsl(0, 36%, 70%)');
@@ -23,14 +23,14 @@ paragraph.style.fontSize = '19px';
 paragraph.style.marginTop = '-30px';
 paragraph.style.lineHeight = ('2');
 
-const inner = document.querySelector('.input');
-inner.style.display = 'flex';
-inner.style.justifyContent = 'space-between';
-inner.style.ilignItems = 'center';
-inner.style.paddingLeft = '10px';
-inner.style.border = '1px #cccccc solid';
-inner.style.borderRadius  = '30px';
-inner.style.marginTop = '30px';
+const button = document.querySelector('.input');
+button.style.display = 'flex';
+button.style.justifyContent = 'space-between';
+button.style.ilignItems = 'center';
+button.style.paddingLeft = '10px';
+button.style.border = '1px #cccccc solid';
+button.style.borderRadius  = '30px';
+button.style.marginTop = '30px';
 
 const input = document.querySelector('.email');
 input.style.border = 'none';
@@ -41,5 +41,11 @@ image.style.backgroundColor = '#ef9e9b';
 image.style.padding = '10px 30px';
 image.style.borderRadius = '30px';
 
+document.querySelector('.button').addEventListener("click", function() {
+    let error = document.querySelector('.error');
+    error.style.display = 'block';
+});
+
 const box = document.querySelector('.maintext');
 box.style.marginLeft = '-50px';
+
